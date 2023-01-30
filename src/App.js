adfa43500453020597b1bcdb4db6e1b08c22bd91
piyush4for <piyush4for@gmail.com>
2023-01-30 18:25:22 +0000

    like counter

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import "./styles.css";
 
 var text = "piyush";
 var color = "blue";
+var likeCounter = 0;
+
+function likeClicked() {
+  likeCounter++;
+  console.log(likeCounter);
+}
+
 export default function App() {
   return (
     <div className="App">
       <h1>
         welcome <span style={{ color: "blue" }}>{text}</span>
       </h1>
+      <button onClick={likeClicked}>Like {likeCounter}</button>
     </div>
   );
 }
